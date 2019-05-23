9c687a77e1dd6a8caf0ca242c75b59bc4c7ef5c8
freedrone <erancihan@gmail.com>
2019-05-23 21:58:36 +0300

    add java-end cors config

--- world/main.go
+++ world/main.go
@@ -22,7 +22,7 @@ func main() {
 	e := echo.New()             // echo instance
 	e.Use(middleware.Recover()) // middleware
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:3000"},
+		AllowOrigins: []string{"http://localhost:3000", "http://localhost:8080"},
 		AllowHeaders: []string{
 			echo.HeaderAccessControlAllowOrigin,
 			echo.HeaderOrigin,
