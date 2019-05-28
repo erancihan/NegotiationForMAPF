a1d082ff68baf7f6337d236708eb9927641eec70
freedrone <erancihan@gmail.com>
2019-05-29 02:50:36 +0300

    show fov grid

--- web-ui/src/components/Watch/display.js
+++ web-ui/src/components/Watch/display.js
@@ -1,18 +1,76 @@
-import React, { useState, forwardRef, useImperativeHandle } from 'react';
+import React, {
+  useEffect,
+  useState,
+  forwardRef,
+  useImperativeHandle
+} from 'react';
+import '@fortawesome/fontawesome-free/svgs/solid/circle.svg';
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
+
+import './display.css';
+
+const overlay = text => <Tooltip id={`tooltip-${text}`}>{text}</Tooltip>;
 
 let Display = function(props, ref) {
   const [data, setData] = useState({
+    agent_id: '',
+    position: '',
     fov_size: 0,
     fov: []
   });
+  const [grid, setGrid] = useState([]);
 
   useImperativeHandle(ref, () => ({
-    pass: d => {
-      setData(d);
-    }
+    pass: d => setData(d)
   }));
 
-  return <div className="col">{}</div>;
+  useEffect(() => {
+    let _grid = new Array(data.fov_size);
+    for (let i = 0; i < data.fov_size; i++) {
+      _grid[i] = new Array(data.fov_size).fill('');
+    }
+
+    let fov = data.fov;
+    if (fov && fov.length > 0) {
+      fov.push([data.agent_id, data.position]);
+
+      fov.map(value => {
+        const xy = value[1].split(':');
+        const x = xy[0];
+        const y = xy[1];
+
+        _grid[x][y] = value[0];
+      });
+    }
+
+    setGrid(_grid);
+  }, [data]);
+
+  // todo colors
+  // todo positional alignments
+
+  return (
+    <div className="col">
+      <table className="table-bordered mx-auto g-display">
+        <tbody>
+          {grid &&
+            grid.map((col, i) => (
+              <tr key={i}>
+                {col.map((v, j) => (
+                  <td key={`${i}:${j}`}>
+                    {v && (
+                      <OverlayTrigger placement="bottom" overlay={overlay(v)}>
+                        <i className="fas fa-circle" />
+                      </OverlayTrigger>
+                    )}
+                  </td>
+                ))}
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 Display = forwardRef(Display);
