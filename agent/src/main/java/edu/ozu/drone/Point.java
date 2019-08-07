8023d00977f0933ab15d592585b035cd0b2abb94
freedrone <erancihan@gmail.com>
2019-08-07 04:27:48 +0300

    add key and manhattan dist calculation to another point

--- agent/src/main/java/edu/ozu/drone/Point.java
+++ agent/src/main/java/edu/ozu/drone/Point.java
@@ -1,11 +1,17 @@
 package edu.ozu.drone;
 
 public class Point {
-    public int x;
-    public int y;
+    public int x, y;
+    public String key;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+
+        this.key = x + "-" + y;
+    }
+
+    public double ManhattanDistTo(Point to) {
+        return Math.abs(x - to.x) + Math.abs(y - to.y);
     }
 }
