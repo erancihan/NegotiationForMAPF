65623da141608b2727cd614c9d005e4a93b50415
freedrone <erancihan@gmail.com>
2019-09-13 20:45:41 +0300

    delete path key too when world terminates

--- world/handler/handler.go
+++ world/handler/handler.go
@@ -71,7 +71,7 @@ func (h *Handler) PlayerUnregister(ctx echo.Context, p *WorldPool) error {
 	}
 
 	if c <= 0 {
-		_, err := rds.Do("DEL", "world:"+wid, "map:world:"+wid)
+		_, err := rds.Do("DEL", "world:"+wid, "map:world:"+wid, "path:world:"+wid)
 		if err != nil {
 			ctx.Echo().Logger.Fatal(err)
 		}
