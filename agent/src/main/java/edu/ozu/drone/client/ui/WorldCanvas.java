266b17ed17625f17b23b4f36e804b3754ad03109
freedrone <erancihan@gmail.com>
2019-09-15 02:55:50 +0300

    improve display of world

--- agent/src/main/java/edu/ozu/drone/client/ui/WorldCanvas.java
+++ agent/src/main/java/edu/ozu/drone/client/ui/WorldCanvas.java
@@ -20,6 +20,7 @@ public class WorldCanvas extends Canvas {
 
     private int r = 0;
     private int fov_center;
+    private int offset = 5; // set offset
 
     @Override
     public void paint(Graphics graphics)
@@ -27,24 +28,11 @@ public class WorldCanvas extends Canvas {
         super.paint(graphics);
         if (data == null || data.fov == null) { return; } // data can be null, dont proceed
 
-        int offset = 5; // set offset
-
         graphics.setColor(Color.WHITE);
         graphics.fillRect(offset, offset, (r * data.fov_size), (r * data.fov_size));
 
-        for (int i = 0; i <= data.fov_size; i++)
-        { // draw vertical lines
-            graphics.setColor(Color.BLACK);
-            graphics.drawLine((offset + i*r), offset, (offset + i*r), (offset + r*data.fov_size));
-        }
-        for (int j = 0; j <= data.fov_size; j++)
-        { // draw horizontal lines
-            graphics.setColor(Color.BLACK);
-            graphics.drawLine(offset, (offset + j*r), (offset + r*data.fov_size), (offset + j*r));
-        }
-
         for (String[] agent_data : data.fov)
-        {
+        { // draw agents
             String name = agent_data[0];
             String[] xy = agent_data[1].split(":");
             String path = agent_data[2];
@@ -57,20 +45,35 @@ public class WorldCanvas extends Canvas {
                 graphics.setColor(new java.awt.Color(39, 145, 60));
             else
                 graphics.setColor(Color.BLUE);
-            graphics.fillOval((offset + (ox*r)), (offset + (oy*r)), r, r);
+            graphics.fillOval((offset + (ox*r) + 5), (offset + (oy*r) + 5), (r - 10), (r - 10));
+        }
+
+        for (int i = 0; i <= data.fov_size; i++)
+        { // draw vertical lines
+            graphics.setColor(Color.BLACK);
+            graphics.drawLine((offset + i*r), offset, (offset + i*r), (offset + r*data.fov_size));
+        }
+        for (int j = 0; j <= data.fov_size; j++)
+        { // draw horizontal lines
+            graphics.setColor(Color.BLACK);
+            graphics.drawLine(offset, (offset + j*r), (offset + r*data.fov_size), (offset + j*r));
         }
 
         if ((xy_own.x - fov_center) < 0 || (xy_own.y - fov_center) < 0)
+        { // draw walls
             for (int y = 0; y < data.fov_size; y++)
                 for (int x = 0; x < data.fov_size; x++)
                 {
                     int cx = xy_own.x - fov_center + x;
                     int cy = xy_own.y - fov_center + y;
-                    if (cx >= 0 && cy >= 0) { continue; }
+                    if (cx >= 0 && cy >= 0) {
+                        continue;
+                    }
 
                     graphics.setColor(new java.awt.Color(56, 56, 56));
-                    graphics.fillRect(offset + (x*r), offset + (y*r), r, r);
+                    graphics.fillRect((offset + (x * r) + 1), (offset + (y * r) + 1), (r - 1), (r - 1));
                 }
+        }
     }
 
     void setData(JSONWorldWatch data, Point agent_position)
@@ -79,8 +82,8 @@ public class WorldCanvas extends Canvas {
         this.xy_own = agent_position;
 
         this.r = this.getWidth() > this.getHeight()
-                ? this.getHeight() / (data.fov_size + 1)
-                : this.getWidth() / (data.fov_size + 1);
+                ? (this.getHeight() - 2 * offset) / (data.fov_size)
+                : (this.getWidth() - 2 * offset) / (data.fov_size);
 
         this.fov_center = data.fov_size / 2;
 
