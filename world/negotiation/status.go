cb6bc8f62a9e1575686317e4c14998e7d2631f6d
freedrone <erancihan@gmail.com>
2019-09-22 22:10:48 +0300

    move session status

--- world/negotiation/status.go
+++ world/negotiation/status.go
@@ -2,6 +2,14 @@ package negotiation
 
 import "github.com/labstack/echo/v4"
 
+type Status struct {
+	AgentCount 	int 	`json:"agent_count"`
+	BidOrder 	string	`json:"bid_order"`
+	Bids      []string	`json:"bids"`
+	State		string	`json:"state"`
+	Turn		string	`json:"turn"`
+}
+
 func (n *Handler) UpdateStatus(ctx echo.Context, pool *SessionPool) {
 	// todo
 }
