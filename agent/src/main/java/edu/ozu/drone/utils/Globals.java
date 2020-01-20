5787d454d31b354f9ec6f4817bff2e874b0e8e35
freedrone <erancihan@gmail.com>
2020-01-20 20:48:18 +0300

    init Globals

---
+++ agent/src/main/java/edu/ozu/drone/utils/Globals.java
@@ -0,0 +1,39 @@
+package edu.ozu.drone.utils;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Globals
+{
+    public enum WorldState {
+        JOIN("JOIN"),
+        BROADCAST("BROADCAST"),
+        NEGOTIATE("NEGOTIATE"),
+        MOVE("MOVE");
+
+        String value;
+
+        WorldState(String value)
+        {
+            this.value = value.toUpperCase();
+        }
+
+        @Override
+        public String toString()
+        {
+            return value;
+        }
+    }
+
+    public static final Map<Integer, WorldState> WORLD_STATES = Collections.unmodifiableMap(
+            new HashMap<Integer, WorldState>() {
+                {
+                    put(0, WorldState.JOIN);
+                    put(1, WorldState.BROADCAST);
+                    put(2, WorldState.NEGOTIATE);
+                    put(3, WorldState.MOVE);
+                }
+            }
+    );
+}
