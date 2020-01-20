30fa091c2cdc741129f30154690dbd41a99c4b9f
freedrone <erancihan@gmail.com>
2020-01-20 16:00:11 +0300

    init Action class

--- agent/src/main/java/edu/ozu/drone/utils/Action.java
+++ agent/src/main/java/edu/ozu/drone/utils/Action.java
@@ -1,4 +1,15 @@
 package edu.ozu.drone.utils;
 
-public class Action {
+public class Action
+{  // action to take
+
+    public Action()
+    {
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
