6632577b1c8958ce3f1bc983f475d4aaf45c005e
freedrone <erancihan@gmail.com>
2020-01-21 06:29:26 +0300

    add get and post http request functions

--- agent/src/main/java/edu/ozu/drone/utils/Utils.java
+++ agent/src/main/java/edu/ozu/drone/utils/Utils.java
@@ -1,6 +1,84 @@
 package edu.ozu.drone.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
 public class Utils {
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Utils.class);
+
+    public static String get(String path)
+    {
+        try {
+            URL url = new URL(path);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String il;
+            StringBuffer response = new StringBuffer();
+            while ((il = in.readLine()) != null) {
+                response.append(il);
+            }
+
+            return String.valueOf(response);
+        } catch (IOException error) {
+            if (error.getClass().getName().equals("java.net.ConnectException")) {
+                logger.error("«check server status»");
+            } else {
+                error.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    public static String post(String path, HashMap<String, String> payload)
+    {
+        try {
+            Gson gson = new GsonBuilder().create();
+            String post_data = gson.toJson(payload);
+
+            URL url = new URL(path);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+
+            // write to output stream
+            try (OutputStream stream = conn.getOutputStream()) {
+                byte[] bytes = post_data.getBytes(StandardCharsets.UTF_8);
+                stream.write(bytes, 0, bytes.length);
+            }
+
+            // read response
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+            String line;
+            StringBuilder response = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            return String.valueOf(response);
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+
+        return null;
+    }
+
     public static String toString(Object[] a) {
         return  toString(a, ", ");
     }
