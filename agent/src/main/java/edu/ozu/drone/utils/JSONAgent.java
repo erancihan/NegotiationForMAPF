f6dd39d27713e27a02f3d11458e187a5dd1a841c
freedrone <erancihan@gmail.com>
2020-01-24 01:09:58 +0300

    good heavens god

--- agent/src/main/java/edu/ozu/drone/utils/JSONAgent.java
+++ agent/src/main/java/edu/ozu/drone/utils/JSONAgent.java
@@ -4,8 +4,8 @@ import com.google.gson.annotations.SerializedName;
 
 public class JSONAgent {
     @SerializedName("agent_id") public String agent_id;
-    @SerializedName("agent_y") public String agent_x;
-    @SerializedName("agent_x") public String agent_y;
+    @SerializedName("agent_x") public String agent_x;
+    @SerializedName("agent_y") public String agent_y;
 
     @Override
     public String toString() {
