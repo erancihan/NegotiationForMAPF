49a707ccb2f125795f36dd486e5a36c469a7b885
freedrone <erancihan@gmail.com>
2020-01-27 01:37:20 +0300

    sample usages

--- agent/src/main/java/edu/ozu/drone/agent/sample/HelloAgent.java
+++ agent/src/main/java/edu/ozu/drone/agent/sample/HelloAgent.java
@@ -2,9 +2,12 @@ package edu.ozu.drone.agent.sample;
 
 import edu.ozu.drone.client.AgentClient;
 import edu.ozu.drone.agent.Agent;
-import edu.ozu.drone.utils.Action;
-import edu.ozu.drone.utils.ActionType;
-import edu.ozu.drone.utils.Point;
+import edu.ozu.drone.client.handlers.World;
+import edu.ozu.drone.utils.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HelloAgent extends Agent
 {
@@ -16,14 +19,38 @@ public class HelloAgent extends Agent
         AGENT_NAME = "Hello Agent";
         AGENT_ID   = "S006486";
 
-        START = new Point(0, 1);
-        DEST = new Point(0, 10);
+        START = new Point(2, 0);
+        DEST = new Point(2, 10);
     }
 
     @Override
     public Action onMakeAction()
     {
-        return new Action(ActionType.OFFER, "");
+        String[][] fov = World.getFieldOfView(this.WORLD_ID, this.AGENT_ID);
+//        System.out.println("fov ha1 > " + Arrays.deepToString(fov));
+
+        ArrayList<String[]> constraints = new ArrayList<>();
+        for (String[] broadcast_data : fov)
+        {
+            String[] broadcast = broadcast_data[2].replaceAll("([\\[\\]]*)", "").split(",");
+            for (int i = 0; i < broadcast.length; i++)
+            {
+                // if target is not the agent in negotiation
+                // opponent id can be extracted from negotiation state
+                // >> skipping for now
+                constraints.add(new String[]{broadcast[i], String.valueOf(i)});
+            }
+        }
+
+        List<String> path = AStar.calculateWithConstraints(POS, DEST, constraints.toArray(new String[0][3]));
+        String[] bid = new String[5];
+        for (int i = 0; i < bid.length; i++) {
+            bid[i] = path.get(i);
+        }
+
+        String _bid = Utils.toString(bid, ",");
+
+        return new Action(ActionType.OFFER, _bid);
     }
 
     @Override
--- agent/src/main/java/edu/ozu/drone/agent/sample/HelloAgent2.java
+++ agent/src/main/java/edu/ozu/drone/agent/sample/HelloAgent2.java
@@ -2,9 +2,12 @@ package edu.ozu.drone.agent.sample;
 
 import edu.ozu.drone.agent.Agent;
 import edu.ozu.drone.client.AgentClient;
-import edu.ozu.drone.utils.Action;
-import edu.ozu.drone.utils.ActionType;
-import edu.ozu.drone.utils.Point;
+import edu.ozu.drone.client.handlers.World;
+import edu.ozu.drone.utils.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HelloAgent2 extends Agent {
 
@@ -13,14 +16,42 @@ public class HelloAgent2 extends Agent {
         AGENT_NAME = "Hello Agent2";
         AGENT_ID   = "S006487";
 
-        START = new Point(1, 0);
-        DEST = new Point(10, 0);
+        START = new Point(0, 2);
+        DEST = new Point(10, 2);
     }
+
+    private int count = 0;
 
     @Override
     public Action onMakeAction()
     {
-        return new Action(ActionType.ACCEPT);
+        if (count++ >= 2)
+            return new Action(ActionType.ACCEPT);
+
+        String[][] fov = World.getFieldOfView(this.WORLD_ID, this.AGENT_ID);
+
+        ArrayList<String[]> constraints = new ArrayList<>();
+        for (String[] broadcast_data : fov)
+        {
+            String[] broadcast = broadcast_data[2].replaceAll("([\\[\\]]*)", "").split(",");
+            for (int i = 0; i < broadcast.length; i++)
+            {
+                // if target is not the agent in negotiation
+                // opponent id can be extracted from negotiation state
+                // >> skipping for now
+                constraints.add(new String[]{broadcast[i], String.valueOf(i)});
+            }
+        }
+
+        List<String> path = AStar.calculateWithConstraints(POS, DEST, constraints.toArray(new String[0][3]));
+        String[] bid = new String[5];
+        for (int i = 0; i < bid.length; i++) {
+            bid[i] = path.get(i);
+        }
+
+        String _bid = Utils.toString(bid, ",");
+
+        return new Action(ActionType.OFFER, _bid);
     }
 
     @Override
