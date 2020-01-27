9442dd24f5d6c5fe5d933a9c4ba174cf9812c3d3
freedrone <erancihan@gmail.com>
2020-01-27 20:21:23 +0300

    add type field

--- world/negotiation/postRequests.go
+++ world/negotiation/postRequests.go
@@ -18,6 +18,7 @@ type (
 		AgentID   string `json:"agent_id" form:"agent_id" query:"agent_id"`
 		SessionID string `json:"session_id" form:"session_id" query:"session_id"`
 		Bid       string `json:"bid" form:"bid" query:"bid"`
+		Type      string `json:"type" form:"type" query:"type"`
 	}
 )
 
