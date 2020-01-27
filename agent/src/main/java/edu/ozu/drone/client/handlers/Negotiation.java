ed21b52f3a03a6ffca7afca068fc07da965ac146
freedrone <erancihan@gmail.com>
2020-01-27 19:49:53 +0300

    fetch active bids

--- agent/src/main/java/edu/ozu/drone/client/handlers/Negotiation.java
+++ agent/src/main/java/edu/ozu/drone/client/handlers/Negotiation.java
@@ -1,5 +1,8 @@
 package edu.ozu.drone.client.handlers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Negotiation {
     private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Negotiation.class);
     private static redis.clients.jedis.Jedis jedis = JedisConnection.getInstance();
@@ -20,5 +23,24 @@ public class Negotiation {
         }
 
         return new String[0];
+    }
+
+    public static String getActiveBid(String WORLD_ID, String AGENT_ID)
+    {
+        Map<String, String> sess = new HashMap<>();
+        try {
+            String session_id = jedis.hget("world:"+WORLD_ID+":notify", "agent:"+AGENT_ID);
+            if (session_id.split(",").length > 1) {
+                logger.error("something went wrong!");
+                System.exit(1);
+            }
+
+            sess = jedis.hgetAll("negotiation:"+session_id);
+            System.out.println(sess);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return sess.getOrDefault("bid:agent:"+AGENT_ID, "[]:0");
     }
 }
