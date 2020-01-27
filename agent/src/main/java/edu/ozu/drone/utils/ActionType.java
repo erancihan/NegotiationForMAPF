40665b5527f6cde2c8b064f300185ad40cf97216
freedrone <erancihan@gmail.com>
2020-01-27 16:24:05 +0300

    add value for print

--- agent/src/main/java/edu/ozu/drone/utils/ActionType.java
+++ agent/src/main/java/edu/ozu/drone/utils/ActionType.java
@@ -1,5 +1,16 @@
 package edu.ozu.drone.utils;
 
 public enum ActionType {
-    ACCEPT, OFFER
+    ACCEPT("ACCEPT"), OFFER("OFFER");
+
+    String value;
+
+    ActionType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
