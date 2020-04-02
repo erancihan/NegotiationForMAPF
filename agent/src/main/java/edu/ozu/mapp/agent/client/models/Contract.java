9ae91c9d708d363f1baada81a46d943aa180784c
freedrone <erancihan@gmail.com>
2020-04-02 07:22:21 +0300

    implement apply function

--- agent/src/main/java/edu/ozu/mapp/agent/client/models/Contract.java
+++ agent/src/main/java/edu/ozu/mapp/agent/client/models/Contract.java
@@ -1,6 +1,8 @@
 package edu.ozu.mapp.agent.client.models;
 
 import edu.ozu.mapp.agent.Agent;
+import edu.ozu.mapp.agent.client.NegotiationSession;
+import edu.ozu.mapp.agent.client.handlers.JedisConnection;
 import edu.ozu.mapp.keys.KeyHandler;
 
 import java.util.Map;
@@ -12,12 +14,15 @@ public class Contract {
     public String a; // id of agent A
     private String ETb;
     public String b; // id of agent B
+    private String sess_id;
 
     public Contract(Map<String, String> sess) {
         Ox = sess.getOrDefault("Ox", "");
         x = sess.getOrDefault("x", "");
         ETa = sess.getOrDefault("ETa", "");
         ETb = sess.getOrDefault("ETb", "");
+
+        sess_id = sess.get("_session_id");
     }
 
     public String getETa(String AgentID) {
@@ -39,12 +44,14 @@ public class Contract {
         return "";
     }
 
-    public void setToken(Agent agent, int next) {
+    public void set(Agent agent, String O, int next) {
         if (a.equals(agent.AGENT_ID))
         {
             int current = Integer.parseInt(getETa(a));
             if (current < next)
             {
+                Ox = O;
+                x = agent.AGENT_ID;
                 ETa = KeyHandler.encrypt(String.valueOf(next), agent.keys.getPrivateKey(agent));
             }
         }
@@ -54,12 +61,21 @@ public class Contract {
             int current = Integer.parseInt(getETb(b));
             if (current < next)
             {
+                Ox = O;
+                x = agent.AGENT_ID;
                 ETb = KeyHandler.encrypt(String.valueOf(next), agent.keys.getPrivateKey(agent));
             }
         }
     }
 
-    public void apply() {
+    public void apply(NegotiationSession session) {
+        if (session.getActiveState().equals("run")) {
+            redis.clients.jedis.Jedis jedis = JedisConnection.getInstance();
 
+            jedis.hset("negotiation:"+sess_id, "Ox", Ox);
+            jedis.hset("negotiation:"+sess_id, "x", x);
+            jedis.hset("negotiation:"+sess_id, "ETa", ETa);
+            jedis.hset("negotiation:"+sess_id, "ETb", ETb);
+        }
     }
 }
