cea33cdf6c58c4dac125a86e1ed1d5163ba8faef
freedrone <erancihan@gmail.com>
2020-04-02 07:24:04 +0300

    update make offer design

--- agent/src/main/java/edu/ozu/mapp/utils/Action.java
+++ agent/src/main/java/edu/ozu/mapp/utils/Action.java
@@ -3,13 +3,14 @@ package edu.ozu.mapp.utils;
 import edu.ozu.mapp.agent.Agent;
 import edu.ozu.mapp.agent.client.handlers.Negotiation;
 import edu.ozu.mapp.agent.client.handlers.World;
+import edu.ozu.mapp.agent.client.models.Contract;
 
 public class Action
 {
     private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Action.class);
 
     public ActionType type;
-    public String bid;
+    public Contract bid;
     private Agent agent;
 
     public Action(Agent agent, ActionType type)
@@ -28,45 +29,53 @@ public class Action
 
         if (type == ActionType.OFFER)
         {
-            if(bid.length() == 0)
-            {
-                logger.error("Bid cannot be empty for Action Type " + type.toString());
-                System.exit(1);
-            }
-            int agent_tokens = World.getTokenBalance(agent.WORLD_ID, agent.AGENT_ID);
-            String[] current_bid_data = Negotiation.getActiveBid(agent.WORLD_ID, agent.AGENT_ID).split(":");
-            if (current_bid_data.length != 2) {
-                logger.error("something went wrong");
-                System.exit(1);
-            }
-            int current_token_offer = Integer.parseInt(current_bid_data[1]);
-            String current_bid_path = current_bid_data[0];
-
-            // if previous bid is the same as current bid,
-            if (current_bid_path.equals(bid))
-                current_token_offer++;
-
-            // if current bid is the same as broadcast, demand more tokens
-            if (agent.getBroadcast().equals(bid))
-                current_token_offer++;
-
-            if (agent_tokens < current_token_offer) {
-                logger.error("cannot demand same path without tokens: " + agent_tokens + " < " + current_token_offer);
-                System.exit(1);
-            }
-            this.bid = bid + ":" + current_token_offer;
+            this.bid = makeOffer(agent, type, bid);
         }
         if (type == ActionType.ACCEPT)
         {
-            this.bid = Negotiation.getActiveBid(agent.WORLD_ID, agent.AGENT_ID);
+            this.bid = Negotiation.getContract(agent.WORLD_ID, agent.AGENT_ID);
         }
         this.type = type;
 
         logger.debug("taking action " + this.type + " - " + this.bid);
     }
 
+    private Contract makeOffer(Agent agent, ActionType type, String bid)
+    {
+        int owned_tokens = World.getTokenBalance(agent.WORLD_ID, agent.AGENT_ID);   // get own token balance
+
+        Contract contract = Negotiation.getContract(agent.WORLD_ID, agent.AGENT_ID);
+
+        int own_token_offer = Integer.parseInt(contract.getToken(agent.AGENT_ID)); // get own bid token count
+
+        boolean will_increase_tokens = false;
+        // if current bid is in agents bid history
+        if (agent.getOwnBidHistory().contains(bid)) {
+            will_increase_tokens = true;
+        }
+
+        // if current bid is the same as broadcast, demand more tokens
+        if (agent.getBroadcast().equals(bid)) {
+            will_increase_tokens = true;
+        }
+
+        if (will_increase_tokens) {
+            own_token_offer += 1;
+
+            if (owned_tokens < own_token_offer) {
+                logger.error("cannot demand same path without tokens: " + owned_tokens + " < " + own_token_offer);
+                System.exit(1);
+            }
+
+            contract.set(agent, bid, own_token_offer);
+        }
+
+        return contract;
+    }
+
+
     @Override
     public String toString() {
-        return agent.AGENT_ID + "-" + type.toString().toLowerCase() + "-" + bid;
+        return agent.AGENT_ID + "-" + type.toString().toLowerCase();
     }
 }
