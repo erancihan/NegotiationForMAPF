4fd2d116d1ecf1b23a03be4efc31ee34da969182
freedrone <erancihan@gmail.com>
2020-05-19 02:27:19 +0300

    handle world state listen

--- world_py/world/listen.py
+++ world_py/world/listen.py
@@ -2,6 +2,6 @@ from redis import Redis
 
 
 def world_listen(world_id, redis: Redis):
-    resp = {}
+    resp = redis.hgetall(world_id)
 
-    return resp
+    return str({str(key, 'UTF-8'): str(resp[key], 'UTF-8') for key in resp})
