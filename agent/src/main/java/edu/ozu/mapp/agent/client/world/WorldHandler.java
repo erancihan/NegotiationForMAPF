f342d6f300567ba91c5723aeff880977aeb05392
freedrone <erancihan@gmail.com>
2020-05-19 02:49:12 +0300

    clean up

--- agent/src/main/java/edu/ozu/mapp/agent/client/world/WorldHandler.java
+++ agent/src/main/java/edu/ozu/mapp/agent/client/world/WorldHandler.java
@@ -1,33 +1,15 @@
 package edu.ozu.mapp.agent.client.world;
 
 import edu.ozu.mapp.agent.client.WorldWatchSocketIO;
-import edu.ozu.mapp.utils.Globals;
 import edu.ozu.mapp.utils.Utils;
 
 import java.util.HashMap;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class WorldHandler
 {
     private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(WorldHandler.class);
     private static redis.clients.jedis.Jedis jedis;
-
-    public static RedisListener createWorld(String WID, BiConsumer<String, String> callback)
-    {
-        HashMap<String, Object> payload = new HashMap<>();
-        payload.put("world_id", WID);
-        payload.put("player_count", "0");
-        payload.put("world_state", "0");
-        payload.put("negotiation_count", "0");
-        payload.put("move_action_count", "0");
-        payload.put("time_tick", 0);
-
-        Utils.post("http://localhost:5000/world/create", payload);
-
-        // subscribe(listen) to changes in world key
-        return new RedisListener(Globals.REDIS_HOST, WID, callback);
-    }
 
     public static void deleteWorld(String WID)
     {
@@ -39,7 +21,7 @@ public class WorldHandler
         Utils.post("http://localhost:5000/world/delete", payload);
     }
 
-    public void CreateWorld(String WID, Consumer<String> callback)
+    public WorldWatchSocketIO CreateWorld(String WID, Consumer<String> callback)
     {
         HashMap<String, Object> payload = new HashMap<>();
         payload.put("world_id", WID);
@@ -51,6 +33,6 @@ public class WorldHandler
 
         Utils.post("http://localhost:5000/world/create", payload);
 
-        WorldWatchSocketIO listener = new WorldWatchSocketIO(WID, callback);
+        return new WorldWatchSocketIO(WID, callback);
     }
 }
