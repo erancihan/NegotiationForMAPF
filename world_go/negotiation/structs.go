5a516bf6b1c91252a5006821e8e8eef698a6f7be
freedrone <erancihan@gmail.com>
2020-07-12 18:39:40 +0300

    fix contract struct

--- world_go/negotiation/structs.go
+++ world_go/negotiation/structs.go
@@ -23,9 +23,9 @@ type (
 		Ox string `json:"Ox" form:"Ox" query:"Ox" redis:"Ox"`
 		X string `json:"x" form:"x" query:"x" redis:"x"`
 		ETa string `json:"ETa" form:"ETa" query:"ETa" redis:"ETa"`
-		A string `json:"a" form:"a" query:"a" redis:"a"`
+		A string `json:"a" form:"a" query:"a" redis:"A"`
 		ETb string `json:"ETb" form:"ETb" query:"ETb" redis:"ETb"`
-		B string `json:"b" form:"b" query:"b" redis:"b"`
+		B string `json:"b" form:"b" query:"b" redis:"B"`
 	}
 	Status struct {
 		AgentCount 	int 		`json:"agent_count"`
