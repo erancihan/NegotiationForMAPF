b0873a6c4f1691a49750e0fc9eddfb81fb35bb8c
Cihan Eran <erancihan@gmail.com>
2020-10-08 17:39:18 +0300

    add initial token count

--- agent/src/main/java/edu/ozu/mapp/utils/JSONAgentData.java
+++ agent/src/main/java/edu/ozu/mapp/utils/JSONAgentData.java
@@ -4,18 +4,23 @@ public class JSONAgentData {
     public int id;
     public String agent_name;
     public String agent_class_name;
+    public String initial_token_count;
     public JSONPointData start;
     public JSONPointData dest;
 
     public JSONAgentData() {
     }
-
     public JSONAgentData(int id, String agent_name, String agent_class_name, Point start, Point dest) {
-        this.id = id;
-        this.agent_name = agent_name;
-        this.agent_class_name = agent_class_name;
-        this.start = new JSONPointData(start);
-        this.dest = new JSONPointData(dest);
+        this(id, agent_name, agent_class_name, "", start, dest);
+    }
+
+    public JSONAgentData(int id, String agent_name, String agent_class_name, String initial_token_count, Point start, Point dest) {
+        this.id                     = id;
+        this.agent_name             = agent_name;
+        this.agent_class_name       = agent_class_name;
+        this.initial_token_count    = initial_token_count;
+        this.start                  = new JSONPointData(start);
+        this.dest                   = new JSONPointData(dest);
     }
 
     @Override
