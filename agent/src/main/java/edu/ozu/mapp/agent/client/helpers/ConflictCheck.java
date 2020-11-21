8d77467621d2f58078562ce75e9cd18f44f0b5e8
Cihan Eran <erancihan@gmail.com>
2020-11-21 23:47:38 +0300

    update conflict check functions

--- agent/src/main/java/edu/ozu/mapp/agent/client/helpers/ConflictCheck.java
+++ agent/src/main/java/edu/ozu/mapp/agent/client/helpers/ConflictCheck.java
@@ -1,5 +1,8 @@
 package edu.ozu.mapp.agent.client.helpers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ConflictCheck {
 
     public enum ConflictType
@@ -11,29 +14,47 @@ public class ConflictCheck {
      * Check for conflicts in given two paths.
      * Returns index of conflict, -1 otherwise
      *
-     * @return int
+     * @return ConflictInfo
      * */
-    public ConflictInfo check(String[] own, String[] other)
+    public ConflictInfo[] GetAllConflicts(String[] own, String[] other)
     {
+        HashMap<String, ConflictInfo> conflicts = new HashMap<>();
+
         // check Vertex Conflict
         for (int i = 0; i < own.length && i < other.length; i++)
         {
             if (own[i].equals(other[i]))
             {
-                return new ConflictInfo(i, ConflictType.VertexConflict);
+                conflicts.put(own[i], new ConflictInfo(i, ConflictType.VertexConflict));
+                break;  // finding 1 per path is enough
             }
         }
 
         // check Swap Conflict
-        for (int t = 0; t + 1 < own.length && t < other.length; t++)
+        for (int t = 0; t + 1 < own.length && t + 1 < other.length; t++)
         {
-            if (own[t].equals(other[t]))
+            if (own[t].equals(other[t + 1]) && own[t+1].equals(other[t]))
             {
-                return new ConflictInfo(t, ConflictType.SwapConflict);
+                conflicts.put(own[t]+own[t+1], new ConflictInfo(t, ConflictType.SwapConflict));
+                break;  // finding 1 per path is enough
             }
         }
 
-        return new ConflictInfo(-1, ConflictType.NONE);
+        if (conflicts.size() > 0) {
+            System.out.println(">>" + conflicts);
+        }
+
+        return conflicts.values().toArray(new ConflictInfo[0]);
+    }
+
+    public ConflictInfo check(String[] own, String[] other)
+    {
+        ConflictInfo[] conflicts = GetAllConflicts(own, other);
+        if (conflicts.length == 0) {
+            return new ConflictInfo(ConflictType.NONE);
+        }
+
+        return GetAllConflicts(own, other)[0];
     }
 }
 
