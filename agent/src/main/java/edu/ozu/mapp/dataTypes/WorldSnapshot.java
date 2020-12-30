352650222a94b33fbbed9bf6331697ee0bdd47f8
Cihan Eran <erancihan@gmail.com>
2020-12-30 03:58:08 +0300

    store dimensions, init & toString

--- agent/src/main/java/edu/ozu/mapp/dataTypes/WorldSnapshot.java
+++ agent/src/main/java/edu/ozu/mapp/dataTypes/WorldSnapshot.java
@@ -8,7 +8,32 @@ import java.util.HashSet;
 
 public class WorldSnapshot
 {
+    public int world_width;
+    public int world_height;
+
     public HashSet<String>                      agent_keys;
     public HashMap<String, Point>               locations;
     public HashMap<String, ArrayList<Point>>    paths;
+
+    public WorldSnapshot()
+    {
+        world_width     = 0;
+        world_height    = 0;
+
+        agent_keys = new HashSet<>();
+        locations  = new HashMap<>();
+        paths      = new HashMap<>();
+    }
+
+    @Override
+    public String toString() {
+        return
+        "WorldSnapshot{" +
+            "\n world_width=" + world_width + ", world_height=" + world_height + "," +
+            "\n agent_keys=" + agent_keys + "," +
+            "\n locations=" + locations + "," +
+            "\n paths=" + paths + "" +
+        "\n}"
+        ;
+    }
 }
