510b9cfc79baf7e3f681c1e0300408bb95eea531
Cihan Eran <erancihan@gmail.com>
2020-12-30 05:30:20 +0300

    render optimisations

--- agent/src/main/java/edu/ozu/mapp/agent/client/world/ScenarioCanvas.java
+++ agent/src/main/java/edu/ozu/mapp/agent/client/world/ScenarioCanvas.java
@@ -6,9 +6,11 @@
 package edu.ozu.mapp.agent.client.world;
 
 import edu.ozu.mapp.agent.client.AgentClient;
+import edu.ozu.mapp.system.Colors;
 import edu.ozu.mapp.system.WorldOverseer;
 import edu.ozu.mapp.utils.JSONAgentData;
 import edu.ozu.mapp.utils.JSONWorldData;
+import edu.ozu.mapp.utils.Path;
 import edu.ozu.mapp.utils.Point;
 
 import java.awt.*;
@@ -66,13 +68,14 @@ public class ScenarioCanvas extends javax.swing.JPanel
     private WorldOverseer world_ref;
 
     // { 'AGENT_ID': [ 'X-Y' , ... ] }
-    private HashMap<String, ArrayList<Point>>   history         = new HashMap<>();
-    private HashMap<String, JSONAgentData>      agents_data     = new HashMap<>();
-    private HashMap<String, AgentClient>        agent_refs      = new HashMap<>();
-    private HashMap<String, Point[]>            agents          = new HashMap<>();
-    private HashMap<String, String>             agent_colors    = new HashMap<>();
+    private HashMap<String, Path>           history         = new HashMap<>();
+    private HashMap<String, JSONAgentData>  agents_data     = new HashMap<>();
+    private HashMap<String, AgentClient>    agent_refs      = new HashMap<>();
+    private HashMap<String, Point[]>        agents          = new HashMap<>();
+    private HashMap<String, String>         agent_colors    = new HashMap<>();
 
     private int offset = 1;
+    private String[] agent_keys;
 
     public void SetWorldData(JSONWorldData world_data)
     {
@@ -96,6 +99,7 @@ public class ScenarioCanvas extends javax.swing.JPanel
         }
     }
 
+    @SuppressWarnings("DuplicatedCode")
     @Override
     public void paint(Graphics graphics)
     {
@@ -103,123 +107,95 @@ public class ScenarioCanvas extends javax.swing.JPanel
 
         if (world == null) return;
 
+        // PREPROCESS
         Graphics2D g = (Graphics2D) graphics.create();
+
+        int width   = world.width;
+        int height  = world.height;
+
         int cell_size = calculate_cell_size();
 
         // DRAW CELL BORDERS
-
         for (int i = 0; i <= world.width; i++)
         {
             g.setColor(Color.BLACK);
-            g.drawLine((offset + i * cell_size), offset, (offset + i * cell_size), (offset + cell_size * world.height));
-            g.drawLine(offset, (offset + i * cell_size), (offset + cell_size * world.width), (offset + i * cell_size));
+            g.drawLine((offset + i * cell_size), offset, (offset + i * cell_size), (offset + cell_size * height));
+            g.drawLine(offset, (offset + i * cell_size), (offset + cell_size * width), (offset + i * cell_size));
         }
 
-        for (String agent_key : agents.keySet())
+        for (String agent_key: agent_keys)
         {
-            g.setColor(hex2rgb(agent_colors.get(agent_key)));   // handle color
-
-            // DRAW AGENT TARGET LOCATION
-            g.fillRect(
-                (offset + (agents_data.get(agent_key).dest.x * cell_size) + (int) (cell_size * 0.25)),
-                (offset + (agents_data.get(agent_key).dest.y * cell_size) + (int) (cell_size * 0.25)),
-                (int) (cell_size * 0.5),
-                (int) (cell_size * 0.5)
-            );
-
-            // DRAW AGENT TARGET LOCATION IDENTIFIER
-            String short_key = agent_key.split("_")[1];
-            g.setColor(Color.RED);
-            g.drawString(
-                short_key,
-                ((agents_data.get(agent_key).dest.x * cell_size) + 2),
-                ((agents_data.get(agent_key).dest.y * cell_size) + 13)
-            );
-        }
-
-        for (String agent_key : agents.keySet())
-        {
-            g.setColor(hex2rgb(agent_colors.get(agent_key)));   // handle color
             Point[] broadcast = agents.get(agent_key);
 
-            // DRAW AGENT CURRENT LOCATION
-            if (broadcast.length > 0)
-            {
-                Point agent_current_loc = broadcast[0];
-
-                g.fillOval(
-                    (agent_current_loc.x * cell_size),
-                    (agent_current_loc.y * cell_size),
-                    cell_size, cell_size
-                );
-
-                // DRAW AGENT CURRENT LOCATION IDENTIFIER
-                String short_key = agent_key.split("_")[1];
-                g.setColor(Color.RED);
-                g.drawString(
-                    short_key,
-                    ((agent_current_loc.x * cell_size) + 2),
-                    ((agent_current_loc.y * cell_size) + 13)
-                );
+            Color color = Colors.hex2rgb(agent_colors.get(agent_key));
+            if (color == null) {
+                color = Colors.hex2rgb(get_color(agent_key));
             }
 
-            g.setColor(hex2rgb(agent_colors.get(agent_key)));   // handle color
-
-            // UPDATE AGENT HISTORY
-            ArrayList<Point> path = history.get(agent_key);
-            if (path.size() == 0 || !broadcast[0].equals(path.get(path.size()-1)))
-            {   // current point does not exist in history
-                // add it to history
-                history.get(agent_key).add(broadcast[0]);
-            }
+            g.setColor(color);
+            g.setStroke(new BasicStroke());
 
             ArrayList<Point> agent_planned_path = null;
             if (agent_refs.containsKey(agent_key)) agent_planned_path = agent_refs.get(agent_key).GetAgentPlannedPath();
             if (agent_planned_path == null) agent_planned_path = new ArrayList<>();
 
-            for (int i = 0; i < path.size() || i + 1 < broadcast.length || i < agent_planned_path.size(); i++)
-            {   // draw the path taken up until this point
+            Path path = new Path(agent_planned_path);
+            int T = 0;
+            boolean is_planned = false;
+            for (int i = 0; i < path.size(); i++)
+            {
+                Point curr = path.get(i);
 
-                Stroke normal = new BasicStroke();
-                g.setStroke(normal);
-                if (i < path.size())
-                {   // DRAW AGENT PATH HISTORY
-                    Point from = path.get(i);
-                    Point dest = i + 1 >= path.size() ? broadcast[0] : path.get(i + 1);
+                if (curr.equals(broadcast[0]))
+                {
+                    // will be drawing broadcast path from now on
+                    is_planned = true;
+                    T = i + broadcast.length-1;
 
-                    g.drawLine(
-                        ((from.x * cell_size) + (cell_size / 2)), ((from.y * cell_size) + (cell_size / 2)),
-                        ((dest.x * cell_size) + (cell_size / 2)), ((dest.y * cell_size) + (cell_size / 2))
-                    );
+                    // DRAW AGENT CURRENT LOCATION
+                    g.fillOval((curr.x * cell_size), (curr.y * cell_size), cell_size, cell_size);
+
+                    // DRAW AGENT CURRENT LOCATION IDENTIFIER
+                    g.setColor(Color.RED);
+                    g.drawString(agent_key.split("_")[1], ((curr.x * cell_size) + 2), ((curr.y * cell_size) + 13));
+
+                    g.setColor(color);
                 }
 
-                if (i + 1 < broadcast.length)
-                {   // DRAW AGENT BROADCAST
-                    Point from = broadcast[i];
-                    Point dest = broadcast[i + 1];
-
-                    g.drawLine(
-                        ((from.x * cell_size) + (cell_size / 2)), ((from.y * cell_size) + (cell_size / 2)),
-                        ((dest.x * cell_size) + (cell_size / 2)), ((dest.y * cell_size) + (cell_size / 2))
+                if (i + 1 >= path.size())
+                {
+                    // DRAW AGENT TARGET LOCATION
+                    g.fillRect(
+                        (offset + (curr.x * cell_size) + (int) (cell_size * 0.25)),
+                        (offset + (curr.y * cell_size) + (int) (cell_size * 0.25)),
+                        (int) (cell_size * 0.5),
+                        (int) (cell_size * 0.5)
                     );
+                    continue;
                 }
 
-                Stroke dashed = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 1, new float[]{2f, 0f, 2f}, 2f);
-                g.setStroke(dashed);
-                if (i >= path.size() && i < agent_planned_path.size())
-                {   // DRAW AGENT PLANNED PATH
-                    Point from = agent_planned_path.get(i);
-                    Point dest = i + 1 >= agent_planned_path.size() ? agent_planned_path.get(i) : agent_planned_path.get(i + 1);
-
-                    g.drawLine(
-                        ((from.x * cell_size) + (cell_size / 2)), ((from.y * cell_size) + (cell_size / 2)),
-                        ((dest.x * cell_size) + (cell_size / 2)), ((dest.y * cell_size) + (cell_size / 2))
-                    );
+                // DRAW AGENT PATH
+                if (is_planned && i + 1 > T)
+                {
+                    Stroke dashed = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 1, new float[]{2f, 0f, 2f}, 2f);
+                    g.setStroke(dashed);
                 }
+
+                Point next = path.get(i + 1);
+                g.drawLine(
+                    ((curr.x * cell_size) + (cell_size / 2)), ((curr.y * cell_size) + (cell_size / 2)),
+                    ((next.x * cell_size) + (cell_size / 2)), ((next.y * cell_size) + (cell_size / 2))
+                );
             }
         }
+    }
 
-        g.dispose();
+    private String get_color(String agent_key)
+    {
+        String hex = Colors.get(agent_colors.keySet().size());
+        agent_colors.put(agent_key, hex);
+
+        return hex;
     }
 
     public void Init()
@@ -244,22 +220,18 @@ public class ScenarioCanvas extends javax.swing.JPanel
         if (world_ref == null) return;
 
         agents = world_ref.GetAllBroadcasts();
-        agents.keySet().forEach(agent_name -> history.put(agent_name, new ArrayList<>()));
+        agents.keySet().forEach(agent_name -> {
+            if (!history.containsKey(agent_name)) history.put(agent_name, new Path());
+        });
+        agent_keys = agents.keySet().toArray(new String[0]);
+        Arrays.sort(agent_keys);
 
         this.repaint();
     }
 
-    public void Resize()
-    {
-        if (world != null)
-        {
-//            cell_size = calculate_cell_size();
-        }
-    }
+    public void Resize() { }
 
-    public void Destroy()
-    {
-    }
+    public void Destroy() { }
 
     private Color hex2rgb(String hex)
     {
