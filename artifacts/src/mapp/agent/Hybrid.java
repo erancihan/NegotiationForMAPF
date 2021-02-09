bc7615ce1dc51c135f28ba59d2946697427cd961
Cihan Eran <erancihan@gmail.com>
2021-02-09 04:03:25 +0300

    fix variable types

--- artifacts/src/mapp/agent/Hybrid.java
+++ artifacts/src/mapp/agent/Hybrid.java
@@ -32,14 +32,12 @@ public class Hybrid extends Agent
     @Override
     public Action onMakeAction(Contract contract)
     {
-        int bid_token_count = contract.GetTokenCountOf(this);
+        double bid_token_count = contract.GetTokenCountOf(this);
 
-        int token_rate = (this.current_tokens - bid_token_count) / this.initial_tokens;
-        int token_path = GetMyRemainingPathLength() / this.initial_path;
+        double token_rate = (this.current_tokens - bid_token_count) / this.initial_tokens;
+        double path_rate = (double) GetMyRemainingPathLength() / (double) this.initial_path.size();
 
-        int rate = token_rate / token_path;
-
-        // todo AC-Next
+        double rate = token_rate / path_rate;
 
         if (rate < 1)
         {   // CONCEDE
