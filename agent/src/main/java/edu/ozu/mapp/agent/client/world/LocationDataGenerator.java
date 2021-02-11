b4caa7e383617f98afedc04e24e3259c8b75f93e
Cihan Eran <erancihan@gmail.com>
2021-02-11 03:33:48 +0300

    refactor & add full rng location gen algo

--- agent/src/main/java/edu/ozu/mapp/agent/client/world/LocationDataGenerator.java
+++ agent/src/main/java/edu/ozu/mapp/agent/client/world/LocationDataGenerator.java
@@ -20,6 +20,7 @@ public class LocationDataGenerator
     private HashSet<String>     AgentDestinations   = new HashSet<>();
     private ArrayList<Point[]>  AgentLocationData   = new ArrayList<>();
 
+    int algo = 0;
     int number_of_expected_conflicts;
     WorldConfig world_data;
 
@@ -30,6 +31,75 @@ public class LocationDataGenerator
     }
 
     public ArrayList<Point[]> GenerateAgentLocationData(int width, int height)
+    {
+        switch (algo)
+        {
+            case 1:
+                return __algo1(width, height);
+            case 0:
+            default:
+                return __algo0(width, height);
+        }
+    }
+
+    public ArrayList<Point[]> __algo0(int width, int height)
+    {
+        logger.debug("Generating Agent Locations...");
+        AgentLocationData = new ArrayList<>();
+
+        ArrayList<Point[]> data;
+        int number_of_conflicts_remaining;
+
+        do {
+            number_of_conflicts_remaining = number_of_expected_conflicts;
+            data = new ArrayList<>();
+            AgentStartLocations = new HashSet<>();  // flush
+            AgentDestinations   = new HashSet<>();  // flush
+
+            for (int index = 0; index < world_data.agent_count; )
+            {
+                // select start
+                Point start;
+                Point destination;
+
+                do {
+                    start = new Point(rng.nextInt(width), rng.nextInt(height));
+                } while (
+                    // there are agents starting from this point
+                    AgentStartLocations.contains(start.key) ||
+                    // there are agents within immediate close proximity for start | Premises Clear
+                    !isPremisesClear(start.x, start.y)
+                );
+
+                do {
+                    destination = new Point(rng.nextInt(width), rng.nextInt(height));
+                } while (
+                    // there are agents starting from this point
+                    AgentDestinations.contains(destination.key) ||
+                    // there are agents within immediate close proximity for start | Premises Clear
+                    !isPremisesClear(destination.x, destination.y)
+                );
+
+                if (isPathLengthOk(start, destination))
+                {
+                    AgentStartLocations.add(start.key);
+                    AgentDestinations.add(start.key);
+                    data.add(new Point[]{ start, destination });
+                    index++;
+                }
+            }
+
+            number_of_conflicts_remaining = getNumber_of_conflicts_remaining(number_of_conflicts_remaining, data);
+            logger.debug("# of conflicts left " + number_of_conflicts_remaining);
+        } while (number_of_conflicts_remaining > 0);
+
+        // append to Agent Location Data
+        AgentLocationData.addAll(data);
+
+        return AgentLocationData;
+    }
+
+    public ArrayList<Point[]> __algo1(int width, int height)
     {
         logger.debug("Generating Agent Locations...");
         AgentLocationData = new ArrayList<>();
@@ -79,42 +149,7 @@ public class LocationDataGenerator
                 data.add(new Point[]{start, destination});
             }
 
-            // get number of conflicts
-            HashMap<String, String[]> paths = new HashMap<>();
-            for (int i = 0; i < data.size() && number_of_conflicts_remaining > 0; i++)
-            {
-                Point[] a = data.get(i);
-                String a_key = a[0].key + "-" + a[1].key;
-                String[] a_path;
-
-                if (paths.containsKey(a_key)) {
-                    a_path = paths.get(a_key);
-                } else {
-                    a_path = new AStar().calculate(a[0], a[1]).toArray(new String[0]);
-                    paths.put(a_key, a_path);
-                }
-
-                for (int j = i + 1; j < data.size() && number_of_conflicts_remaining > 0; j++)
-                {
-                    Point[] b = data.get(j);
-                    String b_key = b[0].key + "-" + b[1].key;
-                    String[] b_path;
-
-                    if (paths.containsKey(b_key)) {
-                        b_path = paths.get(b_key);
-                    } else {
-                        b_path = new AStar().calculate(b[0], b[1]).toArray(new String[0]);
-                        paths.put(b_key, b_path);
-                    }
-
-                    ConflictInfo[] info = new ConflictCheck().GetAllConflicts(a_path, b_path);
-                    if (info.length > 0)
-                    {   // there are conflicts
-                        logger.debug(String.format("Found %s conflict(s) between %s - %s", info.length, i, j));
-                        number_of_conflicts_remaining -= info.length;
-                    }
-                }
-            }
+            number_of_conflicts_remaining = getNumber_of_conflicts_remaining(number_of_conflicts_remaining, data);
             logger.debug("# of conflicts left " + number_of_conflicts_remaining);
         } while (number_of_conflicts_remaining > 0);
 
@@ -122,6 +157,48 @@ public class LocationDataGenerator
         AgentLocationData.addAll(data);
 
         return AgentLocationData;
+    }
+
+    private int getNumber_of_conflicts_remaining(int number_of_conflicts_remaining, ArrayList<Point[]> data)
+    {
+        // get number of conflicts
+        HashMap<String, String[]> paths = new HashMap<>();
+        for (int i = 0; i < data.size() && number_of_conflicts_remaining > 0; i++)
+        {
+            Point[] a = data.get(i);
+            String a_key = a[0].key + "-" + a[1].key;
+            String[] a_path;
+
+            if (paths.containsKey(a_key)) {
+                a_path = paths.get(a_key);
+            } else {
+                a_path = new AStar().calculate(a[0], a[1]).toArray(new String[0]);
+                paths.put(a_key, a_path);
+            }
+
+            for (int j = i + 1; j < data.size() && number_of_conflicts_remaining > 0; j++)
+            {
+                Point[] b = data.get(j);
+                String b_key = b[0].key + "-" + b[1].key;
+                String[] b_path;
+
+                if (paths.containsKey(b_key)) {
+                    b_path = paths.get(b_key);
+                } else {
+                    b_path = new AStar().calculate(b[0], b[1]).toArray(new String[0]);
+                    paths.put(b_key, b_path);
+                }
+
+                ConflictInfo[] info = new ConflictCheck().GetAllConflicts(a_path, b_path);
+                if (info.length > 0)
+                {   // there are conflicts
+                    logger.debug(String.format("Found %s conflict(s) between %s - %s", info.length, i, j));
+                    number_of_conflicts_remaining -= info.length;
+                }
+            }
+        }
+
+        return number_of_conflicts_remaining;
     }
 
     private boolean isPremisesClear(int _x, int _y)
@@ -158,12 +235,6 @@ public class LocationDataGenerator
 
         // todo is min smaller than max????????
 
-        return (
-                // distance is less than max
-                (world_data.max_path_len <= 0 || dist <= world_data.max_path_len)
-                &&
-                // distance is greater than min length
-                (dist >= world_data.min_path_len)
-        );
+        return (world_data.min_path_len <= dist && dist <= world_data.max_path_len);
     }
 }
