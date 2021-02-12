8719fa7555e8e19c489cfe36687cdd42022433ad
Cihan Eran <erancihan@gmail.com>
2021-02-13 00:04:58 +0300

    update Sample.java

--- artifacts/src/Sample.java
+++ artifacts/src/Sample.java
@@ -13,9 +13,11 @@ import java.util.ArrayList;
 public class Sample {
     public static void main(String[] args) {
         try {
+            System.out.println(FileSystemView.getFileSystemView().getDefaultDirectory().getPath());
+            System.out.println(System.getProperty("user.dir"));
 //            ScenarioManager.run(args);
 //            new Sample().gen_cases();
-//            new Sample().run_tournament();
+            new Sample().run_tournament();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -23,40 +25,43 @@ public class Sample {
 
     public void gen_cases()
     {
-        int number_of_cases_to_generate = 1;
+        int number_of_cases_to_generate = 5;
         String timestamp = String.valueOf(System.currentTimeMillis());
-
-        WorldConfig world_config = new WorldConfig();
-        world_config.world_id = timestamp;
-        world_config.width    = 32;
-        world_config.height   = 32;
-        world_config.min_path_len = 1;
-        world_config.max_path_len = 100000;
-        world_config.min_distance_between_agents = 1;
-        world_config.agent_count = -1;
-        world_config.initial_token_c = 10;
-        world_config.number_of_expected_conflicts = 2;
-        world_config.instantiation_configuration = new Object[][]{
-                {"mapp.agent.RandomAgent", 40},
-        };
 
         for (int i = 0; i < number_of_cases_to_generate; i++)
         {
+            WorldConfig world_config = new WorldConfig();
+            world_config.world_id = timestamp;
+            world_config.width    = 16;
+            world_config.height   = 16;
+            world_config.min_path_len = 8;
+            world_config.max_path_len = 24;
+            world_config.min_distance_between_agents = 1;
+            world_config.agent_count = -1;
+            world_config.initial_token_c = 10;
+            world_config.number_of_expected_conflicts = 2;
+            world_config.instantiation_configuration = new Object[][]{
+                    {"mapp.agent.RandomAgent", 40},
+            };
+
             ScenarioManager manager = new ScenarioManager(true);
 
-            world_config.world_id = timestamp + "-" + i;
+            world_config.world_id = System.currentTimeMillis() + "-" + i;
             manager
                 .GenerateScenario(world_config)
                 .thenAccept(agentConfigs -> {
                     AgentConfig[] agent_config = agentConfigs.toArray(new AgentConfig[0]);
 
                     manager.SaveScenario(agent_config, world_config);
-                });
+                })
+                .exceptionally(ex -> { ex.printStackTrace(); return null; })
+            ;
         }
     }
 
     public void run_tournament() throws IOException {
-        Path path = Paths.get(FileSystemView.getFileSystemView().getDefaultDirectory().getPath(), "MAPP");
+//        Path path = Paths.get(FileSystemView.getFileSystemView().getDefaultDirectory().getPath(), "MAPP");
+        Path path = Paths.get(System.getProperty("user.dir"), "artifacts", "configs");
         ArrayList<String> scenarios = new Glob().glob(path, "*.json");
 
         new TournamentRunner().run(scenarios);
