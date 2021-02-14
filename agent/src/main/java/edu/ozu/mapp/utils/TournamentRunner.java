cfbd9fc5a544f12ba8f060f6ea846697cfc9782f
Cihan Eran <erancihan@gmail.com>
2021-02-14 17:28:13 +0300

    add private gen_cases func for debug & dev purposes

--- agent/src/main/java/edu/ozu/mapp/utils/TournamentRunner.java
+++ agent/src/main/java/edu/ozu/mapp/utils/TournamentRunner.java
@@ -1,9 +1,12 @@
 package edu.ozu.mapp.utils;
 
 import edu.ozu.mapp.agent.client.world.ScenarioManager;
+import edu.ozu.mapp.config.AgentConfig;
+import edu.ozu.mapp.config.WorldConfig;
 
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -16,14 +19,56 @@ public class TournamentRunner {
             System.out.println(FileSystemView.getFileSystemView().getDefaultDirectory().getPath());
             System.out.println(System.getProperty("user.dir"));
 
-//        Path path = Paths.get(FileSystemView.getFileSystemView().getDefaultDirectory().getPath(), "MAPP");
-            Path path = Paths.get(System.getProperty("user.dir"), "artifacts", "configs");
-            ArrayList<String> scenarios = new Glob().glob(path, "*.json");
-
-            new TournamentRunner().run(scenarios);
+//            new TournamentRunner().gen_cases();
+            new TournamentRunner().run_tournament();
         } catch (Exception exception) {
             exception.printStackTrace();
             System.exit(1);
+        }
+    }
+
+    private void run_tournament() throws IOException
+    {
+//        Path path = Paths.get(FileSystemView.getFileSystemView().getDefaultDirectory().getPath(), "MAPP");
+        Path path = Paths.get(System.getProperty("user.dir"), "artifacts", "configs");
+        ArrayList<String> scenarios = new Glob().glob(path, "*.json");
+
+        new TournamentRunner().run(scenarios);
+    }
+
+    private void gen_cases()
+    {
+        int number_of_cases_to_generate = 5;
+        String timestamp = String.valueOf(System.currentTimeMillis());
+
+        for (int i = 0; i < number_of_cases_to_generate; i++)
+        {
+            WorldConfig world_config = new WorldConfig();
+            world_config.world_id = timestamp;
+            world_config.width    = 16;
+            world_config.height   = 16;
+            world_config.min_path_len = 8;
+            world_config.max_path_len = 24;
+            world_config.min_distance_between_agents = 1;
+            world_config.agent_count = -1;
+            world_config.initial_token_c = 10;
+            world_config.number_of_expected_conflicts = 2;
+            world_config.instantiation_configuration = new Object[][]{
+                    {"mapp.agent.RandomAgent", 40},
+            };
+
+            ScenarioManager manager = new ScenarioManager(true);
+
+            world_config.world_id = System.currentTimeMillis() + "-" + i;
+            manager
+                    .GenerateScenario(world_config)
+                    .thenAccept(agentConfigs -> {
+                        AgentConfig[] agent_config = agentConfigs.toArray(new AgentConfig[0]);
+
+                        manager.SaveScenario(agent_config, world_config);
+                    })
+                    .exceptionally(ex -> { ex.printStackTrace(); return null; })
+            ;
         }
     }
 
