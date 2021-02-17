feffe4f892d254963df012ce725170815e2fbf37
Cihan Eran <erancihan@gmail.com>
2021-02-18 02:45:27 +0300

    add get neighbours function

--- agent/src/main/java/edu/ozu/mapp/utils/path/Node.java
+++ agent/src/main/java/edu/ozu/mapp/utils/path/Node.java
@@ -2,9 +2,7 @@ package edu.ozu.mapp.utils.path;
 
 import edu.ozu.mapp.utils.Point;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Node implements Comparable<Node>, Cloneable {
@@ -32,6 +30,76 @@ public class Node implements Comparable<Node>, Cloneable {
     public Node(Point point, double dist, int time) {
         this(point, dist);
         this.time = time;
+    }
+
+    public List<Node> getNeighbours(HashMap<String, ArrayList<String>> constraints, int bound_r, int bound_b)
+    {
+        return getNeighbours(null, constraints, bound_r, bound_b);
+    }
+
+    public List<Node> getNeighbours(Point goal, HashMap<String, ArrayList<String>> constraints, int bound_r, int bound_b)
+    {
+        return getNeighbours(goal, constraints, 0, bound_r, 0, bound_b);
+    }
+
+    public List<Node> getNeighbours(Point goal, HashMap<String, ArrayList<String>> constraints, int bound_l, int bound_r, int bound_t, int bound_b)
+    {
+        List<Node> nodes = new ArrayList<>();
+        for (int i = 0; i < 9; i++)
+        {   // BEGIN:FOR | NEIGHBOURHOOD SEARCH
+            /*
+             * On a grid, iterates & calculates points.
+             *  it can be mapped respectively as following
+             *
+             * Dx := (i % 3) - 1
+             * Dy := (i / 3) - 1
+             *
+             * [ 0 , 1 , 2 ]          [ (-1,-1) , ( 0,-1) , ( 1,-1) ]
+             * [ 3 , 4 , 5 ]    ->    [ (-1, 0) , ( 0, 0) , ( 1, 0) ]
+             * [ 6 , 7 , 8 ]          [ (-1, 1) , ( 0, 1) , ( 1, 1) ]
+             */
+            // skip inter-cardinals & center
+            if (i % 2 == 0) continue;
+
+            int x = point.x + (i % 3) - 1;
+            int y = point.y + (i / 3) - 1;
+
+            if (x < bound_l || bound_r <= x) continue;  // x : [bound_l, bound_r) | i.e. [0, 16) -> x : 0, 1, ... 15
+            if (y < bound_t || bound_b <= y) continue;  // y : [bound_t, bound_b) | i.e. [0, 16) -> y : 0, 1, ... 15
+
+            Point neighbour = new Point(x, y);
+            // Check for Vertex Constraint
+            if (constraints.containsKey(neighbour.key))
+            {
+                // There is an entry in constraints for this location
+                // When navigated to next neighbour, neighbour will be in next time step
+                if (
+                    constraints.get(neighbour.key).contains(String.valueOf(time + 1)) ||
+                    constraints.get(neighbour.key).contains("inf")  // if the location is infinitely occupied
+                ) {
+                    continue;
+                }
+            }
+
+            // Check for Swap Constraint
+            // todo
+
+            // When navigated to next neighbour, they will
+            //  be in the next time step from this node.
+            nodes.add(
+                new Node(
+                    neighbour,
+                    goal == null ? 0 : neighbour.ManhattanDistTo(goal),
+                    (time + 1)
+                )
+            );
+
+        }   // END:FOR
+
+        // Add self for cyclic dep
+//        nodes.add(new Node(point, time + 1));
+
+        return nodes;
     }
 
     @Override
