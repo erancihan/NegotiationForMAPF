ff36f11c7316d02100f162e858249f4d735777da
Cihan Eran <erancihan@gmail.com>
2021-02-19 02:51:49 +0300

    update TournamentRunner

--- agent/src/main/java/edu/ozu/mapp/utils/TournamentRunner.java
+++ agent/src/main/java/edu/ozu/mapp/utils/TournamentRunner.java
@@ -1,5 +1,6 @@
 package edu.ozu.mapp.utils;
 
+import com.google.gson.Gson;
 import edu.ozu.mapp.agent.client.world.ScenarioManager;
 import edu.ozu.mapp.config.AgentConfig;
 import edu.ozu.mapp.config.WorldConfig;
@@ -7,11 +8,14 @@ import edu.ozu.mapp.config.WorldConfig;
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.CountDownLatch;
+import java.util.stream.Stream;
 
 public class TournamentRunner {
     public static void main(String[] arg) {
@@ -20,6 +24,7 @@ public class TournamentRunner {
             System.out.println(System.getProperty("user.dir"));
 
 //            new TournamentRunner().gen_cases();
+//            new TournamentRunner().fetch_cbs_conf();
             new TournamentRunner().run_tournament();
         } catch (Exception exception) {
             exception.printStackTrace();
@@ -30,10 +35,74 @@ public class TournamentRunner {
     private void run_tournament() throws IOException
     {
 //        Path path = Paths.get(FileSystemView.getFileSystemView().getDefaultDirectory().getPath(), "MAPP");
-        Path path = Paths.get(System.getProperty("user.dir"), "artifacts", "configs");
-        ArrayList<String> scenarios = new Glob().glob(path, "*.json");
+        Path path = Paths.get(System.getProperty("user.dir"), "artifacts", "configs", "15agents");
+        ArrayList<String> scenarios = new Glob().glob(path, "world-scenario-*.json");
 
         new TournamentRunner().run(scenarios);
+    }
+
+    @SuppressWarnings("DuplicatedCode")
+    private void fetch_cbs_conf() throws IOException
+    {
+        Gson gson = new Gson();
+
+        Path path = Paths.get(System.getProperty("user.dir"), "artifacts", "configs", "8x8map_0obst", "15agents");
+        ArrayList<String> confs = new Glob().glob(path, "\\**\\*.json");
+
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        for (String config_path : confs)
+        {
+            Path __path = Paths.get(config_path);
+
+            String[] __idx = __path.getFileName().toString().split("\\.")[0].split("_");
+            String idx = __idx[__idx.length-1];
+
+            StringBuilder sb = new StringBuilder();
+            try (Stream<String> stream = Files.lines(__path, StandardCharsets.UTF_8)) {
+                stream.forEach(s -> sb.append(s).append("\n"));
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(500);
+            }
+            CBSConfig config = gson.fromJson(sb.toString(), CBSConfig.class);
+
+            WorldConfig world_config = new WorldConfig();
+            world_config.world_id = timestamp + "-" + idx;
+            world_config.width = config.map.get("dimensions").get(0);
+            world_config.height = config.map.get("dimensions").get(1);
+            world_config.min_path_len = 8;
+            world_config.max_path_len = 24;
+            world_config.min_distance_between_agents = 1;
+            world_config.agent_count = -1;
+            world_config.initial_token_c = 10;
+            world_config.number_of_expected_conflicts = 0;
+            world_config.instantiation_configuration = new Object[][]{
+                    {"mapp.agent.RandomAgent", config.agents.size()},
+            };
+
+            ScenarioManager manager = new ScenarioManager(true);
+            manager
+                    .GenerateScenario(world_config)
+                    .thenAccept(agentConfigs -> {
+                        AgentConfig[] agent_config = agentConfigs.toArray(new AgentConfig[0]);
+                        for (int i = 0; i < agent_config.length; i++) {
+                            ArrayList<Double> start = (ArrayList<Double>) config.agents.get(i).get("start");
+                            agent_config[i].start.x = start.get(0).intValue();
+                            agent_config[i].start.y = start.get(1).intValue();
+
+                            ArrayList<Double> dest = (ArrayList<Double>) config.agents.get(i).get("goal");
+                            agent_config[i].dest.x = dest.get(0).intValue();
+                            agent_config[i].dest.y = dest.get(1).intValue();
+                        }
+
+                        manager.SaveScenario(agent_config, world_config);
+                    })
+                    .exceptionally(ex -> {
+                        ex.printStackTrace();
+                        return null;
+                    })
+            ;
+        }
     }
 
     private void gen_cases()
@@ -86,9 +155,20 @@ public class TournamentRunner {
             ScenarioManager
                 .run(new String[0])
                 .thenApply(manager -> {
-                    manager.OnWindowClosed(latch::countDown);
+                    File __file = new File(scenario);
+                    manager.OnWindowClosed(() -> {
+                        File __new = new File(__file.getParent(), "__" + __file.getName());
+                        try {
+                            Files.move(__file.toPath(), __new.toPath());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            System.exit(500);
+                        }
 
-                    return manager.SetScenario(new File(scenario));
+                        latch.countDown();
+                    });
+
+                    return manager.SetScenario(__file);
                 })
                 .thenApply(manager -> manager.RunScenario(true))
             ;
