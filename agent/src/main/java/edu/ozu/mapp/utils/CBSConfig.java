e211979f680c76ef3b09786c0575240787b32775
Cihan Eran <erancihan@gmail.com>
2021-02-19 02:52:03 +0300

    add cbs config data file

---
+++ agent/src/main/java/edu/ozu/mapp/utils/CBSConfig.java
@@ -0,0 +1,12 @@
+package edu.ozu.mapp.utils;
+
+import com.google.gson.internal.LinkedTreeMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CBSConfig
+{
+    public LinkedTreeMap<String, ArrayList<Integer>> map;
+    public List<LinkedTreeMap<String, Object>> agents;
+}
