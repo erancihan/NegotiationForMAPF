b4eeaf60eac5daca0f67a4728207125266e671bb
Cihan Eran <erancihan@gmail.com>
2021-02-19 01:23:45 +0300

    update glob

--- agent/src/main/java/edu/ozu/mapp/utils/Glob.java
+++ agent/src/main/java/edu/ozu/mapp/utils/Glob.java
@@ -1,36 +1,26 @@
 package edu.ozu.mapp.utils;
 
 import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.HashSet;
 
 public class Glob {
     public ArrayList<String> glob(java.nio.file.Path folder, String glob) throws IOException
     {
         ArrayList<String> matches = new ArrayList<>();
-//        String folder_path = folder.endsWith("\\") ? String.valueOf(folder) : folder + File.separator;
-        String pattern = "glob:" + glob;
+//        String folder_path = folder.endsWith("\\") ? String.valueOf(folder) : folder + java.io.File.separator;
+        String pattern = glob.replace("*", ".*?");
 
-        PathMatcher matcher = FileSystems.getDefault().getPathMatcher(pattern);
-        Files.walkFileTree(folder, new HashSet<>(), 1, new SimpleFileVisitor<java.nio.file.Path>() {
-            @Override
-            public FileVisitResult visitFile(java.nio.file.Path file, BasicFileAttributes attributes) {
-                if (matcher.matches(file.getFileName())) {
+        Files
+            .find(folder, Integer.MAX_VALUE, (filePath, fileAttr) -> fileAttr.isRegularFile())
+            .forEach(file -> {
+                if (file.toFile().getName().matches(pattern))
+                {
                     matches.add(String.valueOf(file));
                 }
+            });
 
-                return FileVisitResult.CONTINUE;
-            }
-
-            @Override
-            public FileVisitResult visitFileFailed(Path file, IOException exc) {
-                return FileVisitResult.CONTINUE;
-            }
-        });
-//        System.out.println(pattern);
+//        System.out.println(matches.size());
 
         return matches;
     }
