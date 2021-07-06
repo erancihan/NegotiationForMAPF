b58b218ca258f46246bc11f76239bb0b171513a6
Cihan Eran <erancihan@gmail.com>
2021-07-06 12:00:59 +0300

    update

--- artifacts/run.ps1
+++ artifacts/run.ps1
@@ -1,2 +1,2 @@
-javac -d .\out --release 8 -cp .\mapp_jar\mapp.jar .\src\Sample.java
-java -classpath .\out\ Sample
+javac -d .\out -cp ".\libs\mapp.jar" .\src\Experiment.java
+java -cp ".\out\;.\libs\mapp.jar" Experiment
