6752214bc62fc53c606496918dda7edc513e6ac0
Cihan Eran <erancihan@gmail.com>
2021-08-03 21:40:34 +0300

    update simulation configuration

--- agent/src/main/java/edu/ozu/mapp/Simulation.java
+++ agent/src/main/java/edu/ozu/mapp/Simulation.java
@@ -16,40 +16,46 @@ import java.util.ArrayList;
 
 public class Simulation {
     @SuppressWarnings("DuplicatedCode")
-    public static void main(String[] args) {
-        try {
-            ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
-            root.setLevel(Level.INFO);
+    public static void main(String[] args)
+    {
+        String[] confs = new String[]{"16x16_20", "16x16_60", "8x8_15", "8x8_20", "16x16_40", "16x16_80"};
 
-            System.out.println(FileSystemView.getFileSystemView().getDefaultDirectory().getPath());
-            System.out.println(System.getProperty("user.dir"));
+        for (String conf : confs)
+        {
+            try
+            {
+                ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+                root.setLevel(Level.INFO);
 
-            Path path = Paths.get(
-                    FileSystemView.getFileSystemView().getDefaultDirectory().getPath(), "MAPP",	// %HOME%/Documents/MAPP
-                    "scenarios",
-                    "HeatMapAgent",
-                    "16x16_40"
-            );
-            ArrayList<String> scenarios = new Glob().glob(path, "world-scenario-*.json");
+                System.out.println(FileSystemView.getFileSystemView().getDefaultDirectory().getPath());
+                System.out.println(System.getProperty("user.dir"));
+
+                Path path = Paths.get(
+                        FileSystemView.getFileSystemView().getDefaultDirectory().getPath(), "MAPP",    // %HOME%/Documents/MAPP
+                        "scenarios",
+                        "HeatMapAgent",
+                        conf
+                );
+                ArrayList<String> scenarios = new Glob().glob(path, "world-scenario-*.json");
 
 // SET FIELD OF VIEW
-            Globals.FIELD_OF_VIEW_SIZE  = 5;     // d = 2 from current location to sides
-            Globals.BROADCAST_SIZE      = 5;     // 2d + 1
+                Globals.FIELD_OF_VIEW_SIZE = 5;     // d = 2 from current location to sides
+                Globals.BROADCAST_SIZE = 5;     // 2d + 1
 //            Globals.FIELD_OF_VIEW_SIZE  = 7;     // d = 3 from current location to sides
 //            Globals.BROADCAST_SIZE      = 7;     // 2d + 1
 //            Globals.FIELD_OF_VIEW_SIZE  = 9;     // d = 4 from current location to sides
 //            Globals.BROADCAST_SIZE      = 9;     // 2d + 1
 
 // SET MOVE ACTION SPACE SIZE
-            Globals.MOVE_ACTION_SPACE_SIZE = 4; //   no wait action
-            // Globals.MOVE_ACTION_SPACE_SIZE = 5; // with wait action
+                Globals.MOVE_ACTION_SPACE_SIZE = 4; //   no wait action
+                // Globals.MOVE_ACTION_SPACE_SIZE = 5; // with wait action
 
 // SET LEAVE ACTION BEHAVIOUR
-            Globals.LEAVE_ACTION_BEHAVIOUR = LeaveActionHandler.LeaveActionTYPE.OBSTACLE;       // obstacle on exit
-            // Globals.LEAVE_ACTION_BEHAVIOUR = LeaveActionHandler.LeaveActionTYPE.PASS_THROUGH; //    leave on exit
+                Globals.LEAVE_ACTION_BEHAVIOUR = LeaveActionHandler.LeaveActionTYPE.OBSTACLE;       // obstacle on exit
+                // Globals.LEAVE_ACTION_BEHAVIOUR = LeaveActionHandler.LeaveActionTYPE.PASS_THROUGH; //    leave on exit
 
 // SET BID SEARCH SPACE OVERRIDE
-            Globals.BID_SEARCH_STRATEGY_OVERRIDE = BidSpace.SearchStrategy.POP_LAST;
+                Globals.BID_SEARCH_STRATEGY_OVERRIDE = BidSpace.SearchStrategy.POP_LAST;
 //            Globals.MAX_BID_SPACE_POOL_SIZE = 100;
 
 // SET WORLD TIMEOUT
@@ -57,23 +63,27 @@ public class Simulation {
 //            Globals.STATE_SIMULATION_PROCESS_COUNTER_LIMIT   = 400;
 
 // TOURNAMENT CONFIG
-            TournamentRunner.TOURNAMENT_RUNNER_MAX_NUMBER_OF_TRIES = 2; // retry once
-            SystemExit.SHUTDOWN_ON_EXIT = false;
+                TournamentRunner.TOURNAMENT_RUNNER_MAX_NUMBER_OF_TRIES = 2; // retry once
+                SystemExit.SHUTDOWN_ON_EXIT = false;
 
-            File tournament_run_results = Paths.get(path.toString(), "runs.txt").toFile();
-            //noinspection ResultOfMethodCallIgnored
-            tournament_run_results.getParentFile().mkdirs();
-            if (!tournament_run_results.exists()) {
+                File tournament_run_results = Paths.get(path.toString(), "runs.txt").toFile();
                 //noinspection ResultOfMethodCallIgnored
-                tournament_run_results.createNewFile();
-            }
+                tournament_run_results.getParentFile().mkdirs();
+                if (!tournament_run_results.exists())
+                {
+                    //noinspection ResultOfMethodCallIgnored
+                    tournament_run_results.createNewFile();
+                }
 
-            TournamentRunner runner = new TournamentRunner();
-            runner.tournament_run_results = tournament_run_results;
-            runner.run(scenarios);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            System.exit(1);
+                TournamentRunner runner = new TournamentRunner();
+                runner.tournament_run_results = tournament_run_results;
+                runner.run(scenarios);
+            }
+            catch (Exception exception)
+            {
+                exception.printStackTrace();
+                System.exit(1);
+            }
         }
     }
 }
